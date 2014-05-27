0be2ef73f4bea0c805ba1ad88b5d01fb77200ef7
Mika <willtard@gmail.com>
2014-05-27 20:09:13 +0200

    Update Home.java

--- src/be/ephec/servlets/home/Home.java
+++ src/be/ephec/servlets/home/Home.java
@@ -6,7 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.IOException;;
 
 @WebServlet("/Home")
 public class Home extends ServletConfig {
